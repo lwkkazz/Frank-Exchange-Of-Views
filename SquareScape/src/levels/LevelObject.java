402bd40932fcddbc41ad37418e210743d97cbcb7
lwkkazz <lucasg.pereira@live.com>
2016-06-27 16:52:32 -0300

    Changed level model

--- SquareScape/src/levels/LevelObject.java
+++ SquareScape/src/levels/LevelObject.java
@@ -1,20 +1,19 @@
 package levels;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
 abstract class LevelObject {
 	
-	public void drawWalls(){
-		
+	
+	public void render(GameContainer gameContainer, StateBasedGame sbGame, Graphics graph){
 	}
-	public void checkWalls(){
-		
+	
+	public void update(GameContainer gameContainer, StateBasedGame sbGame, int delta){
 	}
-	public void triggers(){
-		
-	}
-	public void goal(){
-		
-	}
-	public void tipText(){
+	
+	public void boundBox(){
 		
 	}
 }
